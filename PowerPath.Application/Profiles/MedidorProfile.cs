555e07bb2cb9da3aa0ad7243e5fd463582452060
Gabriel <gabrielinacio@id.uff.br>
2025-02-14 12:31:21 -0300

    Implementação da filtragem de medidores

--- PowerPath.Application/Profiles/MedidorProfile.cs
+++ PowerPath.Application/Profiles/MedidorProfile.cs
@@ -11,18 +11,18 @@ namespace PowerPath.Application.Profiles
         {
             CreateMap<Medidor, MedidorDTO>()
                 .ForMember(d => d.Excluido, opt => opt.MapFrom(o => o.Excluido == 1))
-                .ForMember(d => d.Criacao, opt => opt.MapFrom(o => o.Criacao.ToString("dd/MM/yyyy hh:mm:ss", CultureInfo.InvariantCulture)))
+                .ForMember(d => d.Criacao, opt => opt.MapFrom(o => o.Criacao.ToString("dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture)))
                 .ForMember(d => d.Alteracao, opt => opt.MapFrom(o => o.Alteracao != null ?
-                    o.Alteracao!.Value.ToString("dd/MM/yyyy hh:mm:ss", CultureInfo.InvariantCulture) :
+                    o.Alteracao!.Value.ToString("dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture) :
                     string.Empty));
 
             CreateMap<MedidorDTO, Medidor>()
                 .ForMember(d => d.Excluido, opt => opt.MapFrom(o => o.Excluido.HasValue && o.Excluido.Value ? 1 : 0))
                 .ForMember(d => d.Criacao, opt => opt.MapFrom(o => string.IsNullOrEmpty(o.Criacao) ?
-                    DateTime.ParseExact(o.Criacao!, "dd/MM/yyyy hh:mm:ss", CultureInfo.InvariantCulture) :
+                    DateTime.ParseExact(o.Criacao!, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture) :
                     (DateTime?)null))
                 .ForMember(d => d.Alteracao, opt => opt.MapFrom(o => string.IsNullOrEmpty(o.Alteracao) ?
-                    DateTime.ParseExact(o.Alteracao!, "dd/MM/yyyy hh:mm:ss", CultureInfo.InvariantCulture) :
+                    DateTime.ParseExact(o.Alteracao!, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture) :
                     (DateTime?)null));
         }
     }
--- PowerPath.WebApplication/wwwroot/JavaScript/index.js
+++ PowerPath.WebApplication/wwwroot/JavaScript/index.js
@@ -1,7 +1,58 @@
 ﻿$(function () {
+    ConfigurarSelect2()
+    ConfigurarFiltros()
+})
+
+function ConfigurarSelect2() {
     $('#operadora').select2({
         placeholder: 'Operadora',
         minimumResultsForSearch: Infinity,
         allowClear: true
     })
-})
+}
+
+function ConfigurarFiltros() {
+    $('#instalacao').on('input', FiltrarMedidores)
+    $('#lote').on('input', FiltrarMedidores)
+    $('#operadora').on('change.select2', FiltrarMedidores)
+    $('#fabricante').on('input', FiltrarMedidores)
+    $('#modelo').on('input', FiltrarMedidores)
+    $('#versao').on('input', FiltrarMedidores)
+}
+
+function FiltrarMedidores() {
+    let $medidores = $('#Medidores .Item'),
+        instalacao = $('.Filtros #instalacao').val(),
+        lote = parseInt($('.Filtros #lote').val()),
+        operadora = $('.Filtros #operadora').val(),
+        fabricante = $('.Filtros #fabricante').val(),
+        modelo = parseInt($('.Filtros #modelo').val()),
+        versao = parseInt($('.Filtros #versao').val())
+
+    $medidores.css('display', 'unset')
+    $medidores.each(function () {
+        let $medidor = $(this),
+            exibir = true
+
+        if (instalacao && instalacao !== '')
+            exibir = exibir && $medidor.data('instalacao').includes(instalacao)
+
+        if (lote)
+            exibir = exibir && parseInt($medidor.data('lote')) == lote
+
+        if (operadora && operadora !== '')
+            exibir = exibir && $medidor.data('operadora') == operadora
+
+        if (fabricante && fabricante !== '')
+            exibir = exibir && $medidor.data('fabricante').includes(fabricante)
+
+        if (modelo) 
+            exibir = exibir && parseInt($medidor.data('modelo')) == modelo
+
+        if (versao)
+            exibir = exibir && parseInt($medidor.data('versao')) == versao
+
+        if (!exibir)
+            $medidor.css('display', 'none')
+    })
+}
