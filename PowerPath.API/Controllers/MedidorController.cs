3a29f15929750dc73c171889e6700448a836429a
Gabriel <gabrielinacio@id.uff.br>
2025-02-15 14:47:35 -0300

    Implementação de outras funções para API

--- PowerPath.API/Controllers/MedidorController.cs
+++ PowerPath.API/Controllers/MedidorController.cs
@@ -41,5 +41,53 @@ namespace PowerPath.API.Controllers
                 return BadRequest(rMedidores.Mensagem);
             }
         }
+
+        [HttpDelete("{instalacao}/{lote}")]
+        public IActionResult Delete(string? instalacao, int? lote)
+        {
+            Resposta<MedidorDTO> rMedidores = _medidorAppService.Excluir(instalacao, lote);
+
+            if (rMedidores.IsSucesso)
+            {
+                return Ok("Medidor excluído com sucesso!");
+            }
+            else
+            {
+                return BadRequest(rMedidores.Mensagem);
+            }
+        }
+
+        [HttpPost("Salvar")]
+        public IActionResult Salvar([FromBody] MedidorDTO medidor)
+        {
+            if (medidor.IsAlteracao.HasValue && medidor.IsAlteracao.Value)
+            {
+                Resposta<MedidorDTO> rAlterarMedidor = _medidorAppService.Alterar(medidor.Instalacao, medidor.Lote, medidor.Operadora,
+                    medidor.Fabricante, medidor.Modelo, medidor.Versao);
+
+                if (rAlterarMedidor.IsSucesso)
+                {
+                    return Ok("Medidor alterado com sucesso!");
+                }
+                else
+                {
+                    return BadRequest(rAlterarMedidor.Mensagem);
+                }
+            }
+            else
+            {
+                Resposta<MedidorDTO> rInserirMedidor = _medidorAppService.Inserir(medidor.Instalacao, medidor.Lote, medidor.Operadora,
+                    medidor.Fabricante, medidor.Modelo, medidor.Versao);
+
+                if (rInserirMedidor.IsSucesso)
+                {
+                    return Ok("Medidor inserido com sucesso!");
+                }
+                else
+                {
+                    return BadRequest(rInserirMedidor.Mensagem);
+                }
+            }
+        }
     }
 }
